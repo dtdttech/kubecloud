1796c6b428acc12e3486179a6ec97870b6084d2a
TerminalHermit <commit@terminalhermit.dev>
2025-07-21 19:53:09 +0200

    .

---
+++ modules/default.nix
@@ -0,0 +1,36 @@
+{lib, ...}: {
+  imports = [
+    ./cilium
+    ./traefik
+  ];
+
+  options = with lib; {
+    networking.domain = mkOption {
+      type = types.str;
+    };
+  };
+
+  config = {
+    nixidy = {
+      target.repository = "https://github.com/arnarg/cluster.git";
+
+      chartsDir = ../charts;
+
+      defaults = {
+        syncPolicy = {
+          autoSync = {
+            enable = true;
+            prune = true;
+            selfHeal = true;
+          };
+        };
+
+        helm.transformer = map (lib.kube.removeLabels [
+          "app.kubernetes.io/managed-by"
+          "app.kubernetes.io/version"
+          "helm.sh/chart"
+        ]);
+      };
+    };
+  };
+}
