fdeaa258afe48f501d9b939438812174f34eb4c7
TerminalHermit <commit@terminalhermit.dev>
2025-07-21 19:47:14 +0200

    ..

--- flake.nix
+++ flake.nix
@@ -1,33 +1,86 @@
 {
   description = "My ArgoCD configuration with nixidy.";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixidy.url = "github:arnarg/nixidy";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    nixidy = {
+      url = "github:arnarg/nixidy";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixhelm = {
+      url = "github:farcaller/nixhelm";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
     nixidy,
+    nixhelm,
   }: (flake-utils.lib.eachDefaultSystem (system: let
     pkgs = import nixpkgs {
       inherit system;
     };
   in {
-    # This declares the available nixidy envs.
     nixidyEnvs = nixidy.lib.mkEnvs {
       inherit pkgs;
-
       envs = {
-        # Currently we only have the one dev env.
-        dev.modules = [./env/dev.nix];
+        prod.modules = [
+          ./modules
+          ./env/prod.nix
+        ];
       };
     };
 
     # Handy to have nixidy cli available in the local
     # flake too.
-    packages.nixidy = nixidy.packages.${system}.default;
+    packages = {
+      nixidy = nixidy.packages.${system}.default;
+      generators = {
+        cilium = nixidy.packages.${system}.generators.fromCRD {
+          name = "cilium";
+          src = pkgs.fetchFromGitHub {
+            owner = "cilium";
+            repo = "cilium";
+            rev = "v1.17.5";
+            hash = "sha256-frpu1kJICbZFwmH/KQ2pZHcS2M+XvLvxZpzVxok2eM8=";
+          };
+          crds = [
+            "pkg/k8s/apis/cilium.io/client/crds/v2/ciliumnetworkpolicies.yaml"
+            "pkg/k8s/apis/cilium.io/client/crds/v2/ciliumclusterwidenetworkpolicies.yaml"
+          ];
+        };
+        traefik = nixidy.packages.${system}.generators.fromCRD {
+          name = "traefik";
+          src = nixhelm.chartsDerivations.${system}.traefik.traefik;
+          crds = [
+            "crds/traefik.io_ingressroutes.yaml"
+            "crds/traefik.io_ingressroutetcps.yaml"
+            "crds/traefik.io_ingressrouteudps.yaml"
+            "crds/traefik.io_traefikservices.yaml"
+          ];
+        };
+      };
+    };
+    apps = {
+      generate = {
+        type = "app";
+        program =
+          (pkgs.writeShellScript "generate-modules" ''
+            set -eo pipefail
+            mkdir -p modules/cilium modules/traefik
+            cat ${self.packages.${system}.generators.cilium} > modules/cilium/generated.nix
+            cat ${self.packages.${system}.generators.traefik} > modules/traefik/generated.nix
+          '')
+          .outPath;
+      };
+    };
 
     # Useful development shell with nixidy in path.
     # Run `nix develop` to enter.
