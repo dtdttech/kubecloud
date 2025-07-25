387e0da7cc4677203e584b57ff008812e588bd6d
TerminalHermit <commit@terminalhermit.dev>
2025-07-25 20:51:26 +0200

    way to hacky

--- modules/eso/default.nix
+++ modules/eso/default.nix
@@ -1,16 +1,15 @@
+# main module
 { lib, config, charts, ... }:
-
 let
-  cfg = config.secrets.eso;
+  cfg = config.secrets.external-secrets;
   namespace = "kube-system";
   values = cfg.values;
 in {
-  options.secrets.eso = with lib; {
+  options.secrets.external-secrets = with lib; {
     enable = mkOption {
       type = types.bool;
       default = true;
     };
-
     values = mkOption {
       type = types.attrsOf types.anything;
       default = {};
@@ -18,43 +17,48 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    applications.eso = {
-      config = {
-        inherit namespace;
-
-        helm.releases.eso = {
-          inherit values;
-          chart = charts.external-secrets.external-secrets;
+    nixidy.applicationImports = [ ./generated.nix ];
+    
+    applications.external-secrets = {
+      inherit namespace;
+      
+      helm.releases.external-secrets = {
+        inherit values;
+        chart = charts.external-secrets.external-secrets;
+      };
+      
+      # Now use the alias name that matches generated.nix
+      resources.externalSecrets.passboltDemo = {
+        apiVersion = "external-secrets.io/v1";
+        kind = "ExternalSecret";
+        metadata = {
+          name = "passbolt-demo";
+          namespace = namespace;
         };
-
-        resources.externalSecrets.passboltDemo = {
-          apiVersion = "external-secrets.io/v1";
-          kind = "ExternalSecret";
-          metadata = {
-            name = "passbolt-demo";
-            namespace = namespace;
+        spec = {
+          refreshInterval = "1h";
+          secretStoreRef = {
+            name = "passbolt";
+            kind = "SecretStore";
           };
-          spec = {
-            refreshInterval = "1h";
-            secretStoreRef = {
-              name = "passbolt";
-              kind = "SecretStore";
-            };
-            target.name = "passbolt-example";
-            data = [
-              {
-                secretKey = "full_secret";
-                remoteRef.key = "e22487a8-feb8-4591-95aa-14b193930cb4";
-              }
-              {
-                secretKey = "password_only";
-                remoteRef = {
-                  key = "e22487a8-feb8-4591-95aa-14b193930cb4";
-                  property = "password";
-                };
-              }
-            ];
+          target = {
+            name = "passbolt-example";
           };
+          data = [
+            {
+              secretKey = "full_secret";
+              remoteRef = {
+                key = "e22487a8-feb8-4591-95aa-14b193930cb4";
+              };
+            }
+            {
+              secretKey = "password_only";
+              remoteRef = {
+                key = "e22487a8-feb8-4591-95aa-14b193930cb4";
+                property = "password";
+              };
+            }
+          ];
         };
       };
     };
--- modules/eso/generated.nix
+++ modules/eso/generated.nix
@@ -1,27 +1,44 @@
-{ lib, ... }:
-
-with lib;
-
-{
-  options.resources.externalSecrets = mkOption {
-    type = types.attrsOf (types.submodule {
-      options = {
-        # Accept anything inside the ExternalSecret definition
-        apiVersion = mkOption {
-          type = types.str;
-        };
-        kind = mkOption {
-          type = types.str;
-        };
-        metadata = mkOption {
-          type = types.attrsOf types.anything;
-        };
-        spec = mkOption {
-          type = types.anything;  # <-- the important part: allow anything in spec
-        };
+{ lib, options, config, ... }:
+with lib; {
+  options = {
+    resources = {
+      "external-secrets.io"."v1"."ExternalSecret" = mkOption {
+        description = "ExternalSecret is a type which declares how to fetch the secret data and how to transform it to a Kubernetes Secret.";
+        type = types.attrsOf types.attrs; # You might want to use a proper submodule here
+        default = {};
       };
-    });
-    default = {};
-    description = "Stub CRD definition for ExternalSecrets.";
+    } // {
+      "externalSecrets" = mkOption {
+        description = "ExternalSecret is a type which declares how to fetch the secret data and how to transform it to a Kubernetes Secret.";
+        type = types.attrsOf types.attrs; # You might want to use a proper submodule here
+        default = {};
+      };
+    };
+  };
+
+  config = {
+    types = [
+      {
+        name = "externalsecrets";
+        group = "external-secrets.io";
+        version = "v1";
+        kind = "ExternalSecret";
+        attrName = "externalSecrets";
+      }
+    ];
+
+    resources = {
+      "external-secrets.io"."v1"."ExternalSecret" = 
+        mkAliasDefinitions options.resources."externalSecrets";
+    };
+
+    defaults = [
+      {
+        group = "external-secrets.io";
+        version = "v1";
+        kind = "ExternalSecret";
+        default.metadata.namespace = lib.mkDefault config.namespace;
+      }
+    ];
   };
 }
